c7cb20ff232451fbb55088df584c4973f3cf3ad1
Qudus Adeyemi <adeyemiqudus361@gmail.com>
2022-09-02 16:46:33 +0100

    Pizza Runner solutions

--- Case Study #2- Pizza Runner/pizza_runner.sql
+++ Case Study #2- Pizza Runner/pizza_runner.sql
@@ -1,30 +1,4 @@
- --DATA CLEANING AND TRANSFORMATION
---view data
-SELECT *
-FROM pizza_runner.customer_orders
-
---inspect the exclusions and extras column closely
-SELECT DISTINCT exclusions
-FROM pizza_runner.customer_orders
-
-SELECT DISTINCT extras
-FROM pizza_runner.customer_orders
-
-SELECT DISTINCT extras
-FROM pizza_runner.customer_orders
-WHERE extras is NULL
-
-SELECT DISTINCT exclusions
-FROM pizza_runner.customer_orders
-WHERE exclusions is NULL
-
-SELECT DISTINCT exclusions
-FROM pizza_runner.customer_orders
-WHERE exclusions = ''
-
-SELECT DISTINCT extras
-FROM pizza_runner.customer_orders
-WHERE extras = 'null'
+ --DATA CLEANING AND TRANSFORMATION of customer table
 
 SELECT order_id, customer_id, pizza_id,
 	CASE
@@ -41,6 +15,35 @@ FROM pizza_runner.customer_orders
 SELECT *
 FROM customer_orders
 
+--DATA TRANSFORMATION of runner table
+SELECT order_id,runner_id,
+	CASE 
+        WHEN pickup_time like 'null' THEN null
+		ELSE pickup_time END AS pickup_time_alt,
+	CASE 
+		WHEN distance in ('null', '') THEN null
+		WHEN distance like '%km' THEN TRIM('km' from distance)
+		ELSE distance END AS distance_alt,
+    CASE 
+		WHEN duration in ('null', NULL) THEN null
+		WHEN duration like '%mins' THEN TRIM('mins' from duration)
+		WHEN duration like '%minutes' THEN TRIM('minutes' from duration)
+		WHEN duration like '%minute' THEN TRIM('minute' from duration)
+		ELSE duration END AS duration_alt,
+	CASE 
+        WHEN cancellation in (null, '','null') THEN null
+		ELSE cancellation END AS cancellation_alt
+INTO TEMP TABLE runner_order
+FROM pizza_runner.runner_orders
+
+ALTER TABLE runner_order
+ALTER COLUMN pickup_time_alt TYPE TIMESTAMP USING pickup_time_alt::TIMESTAMP,
+ALTER COLUMN distance_alt TYPE double precision USING distance_alt::double precision,
+ALTER COLUMN duration_alt TYPE INT USING duration_alt:: integer;
+
+-- check runner order table
+SELECT *
+FROM runner_order
 --Questions : Pizza Metrics
 -- Q1: How many pizzas were ordered?
 SELECT count(pizza_id)
@@ -49,23 +52,23 @@ FROM customer_orders
 SELECT COUNT(DISTINCT(order_id)) as unique_order_count
 FROM customer_orders
 --Q3: How many successful orders were delivered by each runner?
-SELECT runner_id, COUNT(order_id)
-FROM runner_order..
-WHERE cancellation_alt = ''
+SELECT runner_id, COUNT(order_id) orders_delivered
+FROM runner_order
+WHERE cancellation_alt is NULL
 GROUP BY 1
 --Q4: How many of each type of pizza was delivered?
-SELECT pizza_name, COUNT(c.pizza_id) no_ofpizza
+SELECT pizza_name, COUNT(c.pizza_id) pizza_count
 FROM customer_orders c
 JOIN runner_order r
 ON c.order_id = r.order_id
 JOIN pizza_runner.pizza_names AS p
 ON c.pizza_id = p.pizza_id
-WHERE cancellation_alt = ''
+WHERE cancellation_alt is NULL
 GROUP BY pizza_name
 
 
 --Q5: How many Vegetarian and Meatlovers were ordered by each customer?
-SELECT customer_id, pizza_name, count(pizza_name) tot_order
+SELECT customer_id, pizza_name, count(pizza_name) total_order
 FROM customer_orders c, pizza_runner.pizza_names p
 WHERE c.pizza_id = p.pizza_id
 GROUP BY 1,2
@@ -73,11 +76,11 @@ ORDER BY 1
 
 --Q6: What was the maximum number of pizzas delivered in a single order? study
 --method 1
-SELECT r.order_id, count(pizza_id) no_pizza
+SELECT r.order_id, count(pizza_id) pizza_count
 FROM customer_orders c
 JOIN runner_order r
 ON c.order_id = r.order_id
-WHERE cancellation_alt = ''
+WHERE cancellation_alt is NULL
 GROUP BY 1
 ORDER BY 2 desc
 LIMIT 1
@@ -88,7 +91,7 @@ SELECT r.order_id, count(pizza_id) no_pizza
 FROM customer_orders c
 JOIN runner_order r
 ON c.order_id = r.order_id
-WHERE cancellation_alt = ''
+WHERE cancellation_alt is NULL
 GROUP BY 1)
 
 SELECT MAX(no_pizza) max_delivered
